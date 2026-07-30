9ed08dc96bbbe37ee7d124bbaa39e24ac59fd329
serendipityMist <serendipitymist@gmail.com>
2026-07-30 18:05:26 +0545

    Add main.go to demonstrate switch statement for day of the week

---
+++ 12_SwitchStatement/main.go
@@ -0,0 +1,29 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	day := 6
+
+	switch day {
+	case 1:
+		fmt.Println("Sunday")
+	case 2:
+		fmt.Println("Monday")
+	case 3:
+		fmt.Println("Tuesday")
+	case 4:
+		fmt.Println("Wednesday")
+	case 5:
+		fmt.Println("Thursday")
+	case 6:
+		fmt.Println("Friday")
+	case 7:
+		fmt.Println("Saturday")
+	default:
+		fmt.Println("Invalid Entered Day number")
+
+	}
+
+}
