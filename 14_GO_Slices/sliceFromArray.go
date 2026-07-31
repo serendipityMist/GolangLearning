ed77b15141a3cc0eaee9be69a35c2fd94a913473
serendipityMist <serendipitymist@gmail.com>
2026-07-31 23:01:25 +0545

    Add sliceFromArray.go to demonstrate slice creation from an array in Go

---
+++ 14_GO_Slices/sliceFromArray.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	myarr := [6]int{10, 11, 12, 13, 14, 15}
+	myslice := myarr[2:4]
+
+	fmt.Printf("myslice = %v\n", myslice)
+	fmt.Printf("length =%d\n", len(myslice))
+	fmt.Printf("capacity =%d\n", cap(myslice))
+}
