2f37e3849782c6e8015c46874194f1582b0dae12
serendipityMist <serendipitymist@gmail.com>
2026-08-02 21:09:26 +0545

    Add main.go to demonstrate slice creation and capacity management in Go

---
+++ 16_len_capacity/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	scores := make([]int, 0, 5)
+
+	scores = append(scores, 100, 200, 300)
+	fmt.Println("After appending 100,200,300", scores, len(scores), cap(scores))
+
+	scores = append(scores, 4000, 500, 533)
+	fmt.Println("After appending 4000,500,533", scores, len(scores), cap(scores))
+
+	scores = append(scores, 123, 124, 125, 126, 890, 99)
+	fmt.Println("After appending more values. To check how much the capcity grows by: ", scores, len(scores), cap(scores))
+}
