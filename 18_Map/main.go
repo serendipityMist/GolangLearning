e080d4e39dda6ef7fcad8531b7deaf930bb00bcb
serendipityMist <serendipitymist@gmail.com>
2026-08-03 21:11:24 +0545

    make method to make the map

--- 18_Map/main.go
+++ 18_Map/main.go
@@ -11,4 +11,20 @@ func main() {
 	fmt.Println("Ages: ", age)
 	fmt.Println("Age of ghan: ", age["ghan"])
 	fmt.Println("Length of age: ", len(age))
+
+	//make to create map
+	//make(map[k]V)
+
+	var scores map[string]int //nil map
+
+	scores = make(map[string]int)
+	scores["math"] = 90
+	scores["science"] = 99
+	scores["Python"] = 100
+
+	fmt.Println(scores)
+	fmt.Println(scores["math"])
+	fmt.Println(scores["science"])
+	fmt.Println(scores["Python"])
+
 }
